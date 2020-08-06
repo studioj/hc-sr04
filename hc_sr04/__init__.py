5d0fc14705d9351d9d04602733aa7fd0c1cb328d
studioj <neefsj@gmail.com>
2020-08-06 03:16:09 +0200

    setting initial pins

--- hc_sr04/__init__.py
+++ hc_sr04/__init__.py
@@ -1,1 +1,11 @@
 from hc_sr04._version import __version__, __version_info__
+import RPi.GPIO as GPIO
+
+
+class HCSR04(object):
+    def __init__(self, trigger, echo):
+        self._trig_pin = trigger
+        self._echo_pin = echo
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self._trig_pin, GPIO.OUT)
+        GPIO.setup(self._echo_pin, GPIO.IN)
--- tests/test_hc_sr04_package.py
+++ tests/test_hc_sr04_package.py
@@ -1,2 +1,47 @@
-def test_the_package_can_be_imported():
-    import hc_sr04
+import unittest
+from unittest.mock import patch, call
+
+# Replace libraries by fake ones
+import sys
+import fake_rpi
+
+sys.modules["RPi"] = fake_rpi.RPi  # Fake RPi
+sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
+sys.modules["smbus"] = fake_rpi.smbus  # Fake smbus (I2C)
+
+from hc_sr04 import HCSR04
+
+
+class TestBasics(unittest.TestCase):
+    def test_by_default_bcm_pins_are_selected(self):
+        trigger = 24
+        echo = 18
+        with patch("hc_sr04.GPIO") as mocked_gpio:
+            HCSR04(trigger, echo)
+            mocked_gpio.setmode.assert_any_call(mocked_gpio.BCM)
+
+    def test_needs_a_trigger_and_an_echo_pin(self):
+        trigger = 24
+        echo = 18
+        sensor = HCSR04(trigger, echo)
+
+        self.assertIs(trigger, sensor._trig_pin)
+        self.assertIs(echo, sensor._echo_pin)
+
+    def test_the_trigger_pin_is_defined_as_an_output(self):
+        # Given
+        trigger = 24
+        echo = 18
+        # When
+        with patch("hc_sr04.GPIO") as mocked_gpio:
+            HCSR04(trigger, echo)
+            mocked_gpio.setup.assert_any_call(trigger, mocked_gpio.OUT)
+
+    def test_the_echo_pin_is_defined_as_an_input(self):
+        # Given
+        trigger = 24
+        echo = 18
+        # When
+        with patch("hc_sr04.GPIO") as mocked_gpio:
+            HCSR04(trigger, echo)
+            mocked_gpio.setup.assert_any_call(echo, mocked_gpio.IN)
