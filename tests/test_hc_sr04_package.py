b2890eca7988c974dc863b7c395f5621bd1de54c
studioj <neefsj@gmail.com>
2020-08-06 22:54:58 +0200

    refactor test

--- tests/test_hc_sr04_package.py
+++ tests/test_hc_sr04_package.py
@@ -7,20 +7,29 @@ import fake_rpi
 
 sys.modules["RPi"] = fake_rpi.RPi  # Fake RPi
 sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
-sys.modules["smbus"] = fake_rpi.smbus  # Fake smbus (I2C)
 
 from hc_sr04 import HCSR04
 
 
 class TestBasics(unittest.TestCase):
-    def test_by_default_bcm_pins_are_selected(self):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.gpio_patcher = patch("hc_sr04.GPIO")
+        cls.gpio_mock = cls.gpio_patcher.start()
+
+    def setUp(self) -> None:
+        self.gpio_mock.reset_mock()
+
+    def test_by_default_bcm_pins_are_selected(self) -> None:
+        # Given
         trigger = 24
         echo = 18
-        with patch("hc_sr04.GPIO") as mocked_gpio:
-            HCSR04(trigger, echo)
-            mocked_gpio.setmode.assert_any_call(mocked_gpio.BCM)
+        # When
+        HCSR04(trigger, echo)
+        # Then
+        self.gpio_mock.setmode.assert_any_call(self.gpio_mock.BCM)
 
-    def test_needs_a_trigger_and_an_echo_pin(self):
+    def test_needs_a_trigger_and_an_echo_pin(self) -> None:
         trigger = 24
         echo = 18
         sensor = HCSR04(trigger, echo)
@@ -28,20 +37,18 @@ class TestBasics(unittest.TestCase):
         self.assertIs(trigger, sensor._trig_pin)
         self.assertIs(echo, sensor._echo_pin)
 
-    def test_the_trigger_pin_is_defined_as_an_output(self):
+    def test_the_trigger_pin_is_defined_as_an_output(self) -> None:
         # Given
         trigger = 24
         echo = 18
         # When
-        with patch("hc_sr04.GPIO") as mocked_gpio:
-            HCSR04(trigger, echo)
-            mocked_gpio.setup.assert_any_call(trigger, mocked_gpio.OUT)
+        HCSR04(trigger, echo)
+        self.gpio_mock.setup.assert_any_call(trigger, self.gpio_mock.OUT)
 
-    def test_the_echo_pin_is_defined_as_an_input(self):
+    def test_the_echo_pin_is_defined_as_an_input(self) -> None:
         # Given
         trigger = 24
         echo = 18
         # When
-        with patch("hc_sr04.GPIO") as mocked_gpio:
-            HCSR04(trigger, echo)
-            mocked_gpio.setup.assert_any_call(echo, mocked_gpio.IN)
+        HCSR04(trigger, echo)
+        self.gpio_mock.setup.assert_any_call(echo, self.gpio_mock.IN)
