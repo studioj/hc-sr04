af5562a3784c504baa25c8024a45c07e563c8c12
studioj <neefsj@gmail.com>
2020-08-06 03:16:09 +0200

    setting initial pins

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
+import sys
 from os import path
 from platform import python_version
 
 from setuptools import setup
+
+try:
+    import RPi.GPIO
+except ImportError:
+    import fake_rpi
+
+    sys.modules["RPi"] = fake_rpi.RPi  # Fake RPi
+    sys.modules["RPi.GPIO"] = fake_rpi.RPi.GPIO  # Fake GPIO
+    sys.modules["smbus"] = fake_rpi.smbus  # Fake smbus (I2C)
 
 from hc_sr04 import __version__
 
